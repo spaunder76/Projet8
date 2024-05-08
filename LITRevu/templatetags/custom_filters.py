7db40856fbeabc8571276211bcff753f120e765c
spaunder76 <arthurbousquet36@gmail.com>
2024-05-08 11:08:53 +0200

    Delete LITRevu/templatetags directory

--- LITRevu/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def is_following_user(user, other_user):
-    return user.following.filter(pk=other_user.pk).exists()
